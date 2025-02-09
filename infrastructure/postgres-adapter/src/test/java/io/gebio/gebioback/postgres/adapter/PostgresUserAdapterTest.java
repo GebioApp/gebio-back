133293e096119f1fd75794d6fcc181e5fa719332
dorian-frances <dorian.frances@ext.bpifrance.fr>
2025-02-09 18:11:27 +0100

    test(31): add adapter test to read user

--- infrastructure/postgres-adapter/src/test/java/io/gebio/gebioback/postgres/adapter/PostgresUserAdapterTest.java
+++ infrastructure/postgres-adapter/src/test/java/io/gebio/gebioback/postgres/adapter/PostgresUserAdapterTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import io.gebio.gebioback.domain.model.User;
 import io.gebio.gebioback.postgres.repository.UserRepository;
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -22,6 +23,22 @@ class PostgresUserAdapterTest extends AbstractPostgresIT {
     User user = postgresUserAdapter.createUserFromMail(email);
 
     assertThat(user).isNotNull();
+    assertThat(user.id()).isNotNull();
+    assertThat(user.email()).hasToString(email);
+  }
+
+  @Test
+  void should_read_user() {
+    PostgresUserAdapter postgresUserAdapter = new PostgresUserAdapter(
+      userRepository
+    );
+    String email = "john.doe@gmail.com";
+    postgresUserAdapter.createUserFromMail(email);
+
+    Optional<User> optionalUser = postgresUserAdapter.findUserByEmail(email);
+
+    assertThat(optionalUser).isNotEmpty();
+    User user = optionalUser.get();
     assertThat(user.id()).isNotNull();
     assertThat(user.email()).hasToString(email);
   }
