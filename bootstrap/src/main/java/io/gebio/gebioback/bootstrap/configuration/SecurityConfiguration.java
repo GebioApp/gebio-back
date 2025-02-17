b1ee88feccc91d077e97666bb849068c4b51dffe
dorian-frances <dfrances.pro@gmail.com>
2025-02-18 00:58:16 +0100

    chore: put security config back to permit only websocket endpoints
    test: fix test

--- bootstrap/src/main/java/io/gebio/gebioback/bootstrap/configuration/SecurityConfiguration.java
+++ bootstrap/src/main/java/io/gebio/gebioback/bootstrap/configuration/SecurityConfiguration.java
@@ -20,7 +20,11 @@ public class SecurityConfiguration {
       .csrf(AbstractHttpConfigurer::disable)
       .cors(cors -> cors.configurationSource(corsConfigurationSource()))
       .authorizeHttpRequests(authorize ->
-        authorize.requestMatchers("/ws/**").permitAll().anyRequest().permitAll()
+        authorize
+          .requestMatchers("/ws/**")
+          .permitAll()
+          .anyRequest()
+          .authenticated()
       )
       .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
       .build();
