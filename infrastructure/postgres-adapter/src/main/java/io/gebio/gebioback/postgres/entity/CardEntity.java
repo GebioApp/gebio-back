a9d3cf8da6989d1bc5aec980ded819392cba5d14
dorian-frances <dfrances.pro@gmail.com>
2025-02-27 19:45:27 +0100

    feat(38): create card entity

---
+++ infrastructure/postgres-adapter/src/main/java/io/gebio/gebioback/postgres/entity/CardEntity.java
@@ -0,0 +1,33 @@
+package io.gebio.gebioback.postgres.entity;
+
+import jakarta.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "card")
+public class CardEntity {
+
+  @Id
+  @Column(name = "id", nullable = false)
+  private UUID id;
+
+  @Column(name = "content", nullable = false)
+  private String content;
+
+  @Column(name = "color", nullable = false)
+  private String color;
+
+  @Column(name = "pos_x", nullable = false)
+  private int posX;
+
+  @Column(name = "pos_y", nullable = false)
+  private int posY;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "owner_id")
+  private UserEntity owner;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "board_id")
+  private BoardEntity board;
+}
