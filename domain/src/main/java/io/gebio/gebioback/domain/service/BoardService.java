78bb1ec29cb71dda690f6b98eee42aa583a9f92e
dorian-frances <dfrances.pro@gmail.com>
2025-03-04 20:33:47 +0100

    refactor(39): better functional coding structure

--- domain/src/main/java/io/gebio/gebioback/domain/service/BoardService.java
+++ domain/src/main/java/io/gebio/gebioback/domain/service/BoardService.java
@@ -6,7 +6,6 @@ import io.gebio.gebioback.domain.model.User;
 import io.gebio.gebioback.domain.port.in.BoardFacade;
 import io.gebio.gebioback.domain.port.out.BoardRepositoryPort;
 import java.util.Collections;
-import java.util.Optional;
 import java.util.UUID;
 import org.springframework.stereotype.Service;
 
@@ -41,12 +40,10 @@ public class BoardService implements BoardFacade {
 
   @Override
   public Board addCardToBoard(UUID boardId, Card card) {
-    Optional<Board> board = boardRepositoryPort.findById(boardId);
-    if (board.isEmpty()) {
-      throw new BoardNotFound(boardId);
-    }
-    Board updatedBoard = Board.addCardToBoard(board.get(), card);
-    boardRepositoryPort.save(updatedBoard);
-    return updatedBoard;
+    return boardRepositoryPort
+      .findById(boardId)
+      .map(board -> Board.addCardToBoard(board, card))
+      .map(boardRepositoryPort::save)
+      .orElseThrow(() -> new BoardNotFound(boardId));
   }
 }
