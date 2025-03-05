be6a4a05cd336aa138a222fbe7ab532715654c89
dorian-frances <dfrances.pro@gmail.com>
2025-03-05 21:10:48 +0100

    fix(39): avoid lazy loading many to one relations and use entity graph to fetch wanted fields

--- infrastructure/postgres-adapter/src/main/java/io/gebio/gebioback/postgres/repository/BoardRepository.java
+++ infrastructure/postgres-adapter/src/main/java/io/gebio/gebioback/postgres/repository/BoardRepository.java
@@ -1,9 +1,14 @@
 package io.gebio.gebioback.postgres.repository;
 
 import io.gebio.gebioback.postgres.entity.BoardEntity;
+import java.util.Optional;
 import java.util.UUID;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BoardRepository extends CrudRepository<BoardEntity, UUID> {}
+public interface BoardRepository extends CrudRepository<BoardEntity, UUID> {
+  @EntityGraph(attributePaths = { "owner", "cards" })
+  Optional<BoardEntity> findById(UUID boardId);
+}
